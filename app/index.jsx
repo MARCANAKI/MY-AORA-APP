eba65b1b79bb5cb85dcdad037abc37f9d145947b
MARCANAKI <augustmarc16@gmail.com>
2025-07-01 22:11:39 +0100

    MyAoraApp

---
+++ app/index.jsx
@@ -0,0 +1,25 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text classname= 'color-lime-400'>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto"/>
+      <Link href= "/profile" style= {{color: "blue"}}>Go to Profile</Link>
+
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
